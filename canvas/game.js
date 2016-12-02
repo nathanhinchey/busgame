bfa46a8d424e947fc4613997c682cb7f4f86a908
Nathan Hinchey <nathanhinchey@gmail.com>
2016-12-02 18:15:48 -0500

    Make a basic paint program in canvas!

--- canvas/game.js
+++ canvas/game.js
@@ -1,8 +1,27 @@
 (function() {
   'use strict';
+  var g = window.spaceGame = window.spaceGame || {};
+  
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   
+  g.drawRect = function(x,y){
+    ctx.fillStyle = 'rgba(20,200,20,0.5)';
+    ctx.strokeStyle = 'white';
+    
+    ctx.fillRect(x - 10, y - 10, 20, 20);
+    ctx.strokeRect(x - 10, y - 10, 20, 20);
+  };
+  
+  g.handleMouseMove = function(event){
+    
+    if (g.mouseDown){
+      g.drawRect(event.pageX, event.pageY);
+    }
+  };
+  
+  document.onmousemove = g.handleMouseMove;
+  document.onmousedown = function(){ g.mouseDown = true; }
+  document.onmouseup = function(){ g.mouseDown = false; }
   
 }());
-  
